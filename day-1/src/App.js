e609bb4f76097f4c698351e26fec6ca7b1cb4227
Ridwan Abdullah <teambacen@gmail.com>
2017-05-07 11:08:38 +0800

    done

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Index from './component/index';
+import './css/master.css';
+import FontAwesome from 'react-fontawesome';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="container">
+          <div className="sidebar">
+            <h1 className="sidebar-title">zora</h1>
+            <div className="sidebar-avatar">
+              <img src="https://placeimg.com/100/100/people" className="img-round"/>
+              <a href="link" className="sidebar-gear">
+                <i className="fa fa-gear"/>
+              </a>
+              <ul>
+                <li className="active"><i className="fa fa-home" aria-hidden="true"></i> <Link to="/test">PROPERTIES</Link></li>
+                <li><i className="fa fa-user-circle-o" aria-hidden="true"></i> <Link to="/test">APPLICANTS</Link></li>
+                <li><i className="fa fa-users" aria-hidden="true"></i> <Link to="/test">TENNANTS</Link></li>
+                <li><i className="fa fa-inbox" aria-hidden="true"></i> <Link to="/test">MESSAGES</Link></li>
+              </ul>
+
+            </div>
+          </div>
+          <div className="content">
+            <Route exact path="/" component={Index}/>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      </Router>
     );
   }
 }
---
+++ day-1/src/component/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import '../css/master.css';
+
+class Index extends React.Component {
+  render () {
+    return(
+      <div className="contain">
+        <div className="card">
+          <div className="card-image">
+            <img src="https://placeimg.com/300/200/nature" className="img-responsive"/>
+          </div>
+          <div className="card-content">
+            <h3>Permai Court 1</h3>
+            <h4>Taman Dato Ahmad Razali</h4>
+          </div>
+          <div className="card-footer">
+            <div className="card-applicant">
+              <h4>6 Applicants</h4>
+            </div>
+            <div className="card-price">
+              <h4>RM 1200</h4>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <img src="https://placeimg.com/300/200/nature" className="img-responsive"/>
+          </div>
+          <div className="card-content">
+            <h3>Permai Court 1</h3>
+            <h4>Taman Dato Ahmad Razali</h4>
+          </div>
+          <div className="card-footer">
+            <div className="card-applicant">
+              <h4>6 Applicants</h4>
+            </div>
+            <div className="card-price">
+              <h4>RM 1200</h4>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <img src="https://placeimg.com/300/200/nature" className="img-responsive"/>
+          </div>
+          <div className="card-content">
+            <h3>Permai Court 1</h3>
+            <h4>Taman Dato Ahmad Razali</h4>
+          </div>
+          <div className="card-footer">
+            <div className="card-applicant">
+              <h4>6 Applicants</h4>
+            </div>
+            <div className="card-price">
+              <h4>RM 1200</h4>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <img src="https://placeimg.com/300/200/nature" className="img-responsive"/>
+          </div>
+          <div className="card-content">
+            <h3>Permai Court 1</h3>
+            <h4>Taman Dato Ahmad Razali</h4>
+          </div>
+          <div className="card-footer">
+            <div className="card-applicant">
+              <h4>6 Applicants</h4>
+            </div>
+            <div className="card-price">
+              <h4>RM 1200</h4>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Index;
